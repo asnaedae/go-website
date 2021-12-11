05e369a4fa121e99eb480a184e9ee90029ca6202
Mike Wallis <mike.wallis@iconsolutions.com>
2021-12-11 20:10:35 +0000

    add chi as a router

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
@@ -15,10 +17,24 @@ func contactHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "<h1>Contact page</h1><p>to get in contact use <a href=\"mailto:mike@dubdubdub.co.uk\">mike@dubdubdub.co.uk</a>")
 }
 
-func main()  {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+func faqHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8")
+	fmt.Fprintf(w, `
+<h1>FAQ page</h1>
+<ul>
+	<li>Q: question 1</li>
+	<li>A: answer 1</li>
+</ul>
+	`)
+}
 
+func main()  {
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 	fmt.Println("Starting server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
